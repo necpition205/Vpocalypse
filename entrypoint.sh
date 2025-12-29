316fa03e00767fb2c6418d6c462a24dc1951cce2
MswTester <mswtester10417@gmail.com>
2025-12-30 02:30:50 +0900

    fix: remove exec from entrypoint.sh to prevent Railway deployment issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ set -e
 
 : "${JVM_OPTS:=-Xms2G -Xmx4G}"
 
-exec java $JVM_OPTS @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.13/unix_args.txt nogui
+java $JVM_OPTS @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.13/unix_args.txt nogui
